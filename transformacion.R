889b309e50c30ff3cb1209d404330b6014d2c668
Alonso <alonmar3@gmail.com>
2020-06-15 19:15:29 -0500

    add layers

--- transformacion.R
+++ transformacion.R
@@ -5,3 +5,8 @@ shp <- st_read("input/shapemunicipios/e20_municipio.shp",
 
 
 st_write(shp,     "output/oaxaca.gpkg", "municipios")
+#st_write(storms, "nc.gpkg", "storms", append = TRUE)
+
+
+shp <- st_read("https://github.com/alonmar/Shapes/blob/master/output/oaxaca.gpkg?raw=true")
+st_layers("https://github.com/alonmar/Shapes/blob/master/output/oaxaca.gpkg?raw=true")
