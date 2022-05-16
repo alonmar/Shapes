9dc524b7e5ded4691fd5b3e169beef6393ea0e3a
Alonso Melgar Lopez <alonmar3@gmail.com>
2022-05-16 11:31:14 -0500

    added ageb urb oaxaca

--- transformacion.R
+++ transformacion.R
@@ -26,3 +26,13 @@ st_write(shp_regiones, "output/oaxaca.gpkg", "regiones_group", append = TRUE)
 
 st_layers("https://github.com/alonmar/Shapes/blob/master/output/oaxaca.gpkg?raw=true")
 shp <- st_read("https://github.com/alonmar/Shapes/blob/master/output/oaxaca.gpkg?raw=true", layer = "regiones_group")
+
+### write Oaxaca AGEB
+
+
+shp <- st_read("../20_oaxaca/conjunto_de_datos/20a.shp", 
+               options = "ENCODING=WINDOWS-1252", 
+               stringsAsFactors = F)
+
+
+st_write(shp,     "output/oaxaca_ageb_urb.gpkg", "ageb_urb")
